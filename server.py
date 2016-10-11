ebf9bced77dcc162340e88a19e15aa04a2d51d7e
gorgunh <gorgunh@itu.edu.tr>
2016-10-11 12:05:28 +0300

    Elephant SQL DNS function and running configurations on localhost has been added.

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 import datetime
 import os
+import json
+import re
 
 from flask import Flask
 from flask import render_template
 
 
 app = Flask(__name__)
+
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 
 @app.route('/')
@@ -20,4 +32,12 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=5432 dbname='itucsdb'"""
+
     app.run(host='0.0.0.0', port=port, debug=debug)
