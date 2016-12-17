f91ae2ad80a5eb81a0b96c85899e457a6dcb371d
ozgunkivrakdal <ozgun.kivrakdal@gmail.com>
2016-12-17 16:33:33 +0200

    implemented handlers of user view that will use user service which is implemented only as an interface

--- user_handler.py
+++ user_handler.py
@@ -1,37 +1,96 @@
 import psycopg2 as dbapi2
-from flask import Blueprint, render_template, url_for , redirect, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, url_for , redirect, request, flash
 from flask.globals import current_app, request
+from flask_login import login_required, logout_user, login_user, current_user
 from user_service import UserService
 from user_class import User
 
+from user_friend_service import UserFriendService
+from user_block_service import UserBlockService
+
 user = Blueprint('user',__name__)
 user.service = UserService()
+user.friend_service = UserFriendService()
+user.block_service = UserBlockService()
 
-@user.route('/user/init')
-def init_user_database():
-    with dbapi2.connect(current_app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """CREATE TABLE users (
-            id bigserial primary key,
-            username varchar(20) unique not null,
-            email varchar(40) unique not null,
-            password bytea not null,
-            full_name varchar(40) not null,
-            gender char,
-            isBlocked char not null
-        )"""
-        cursor.execute(query)
-        connection.commit()
-        return render_template('home.html',**locals())
-@user.route('/ozgun', methods = ['GET', 'POST'])
-def add_user():
+@user.errorhandler(dbapi2.Error)
+def database_exception_handler(error):
+    return 'Database error: '+str(error), 500
+
+@user.route('/users/init')
+def init():
+    user.service.create_table()
+    user.friend_service.create_table()
+    user.block_service.create_table()
+    return redirect(url_for('home_page'))
+
+@user.route('/users/destroy')
+def destroy():
+    user.service.destroy_table()
+    user.friend_service.destroy_table()
+    user.block_service.destroy_table()
+    return redirect(url_for('home_page'))
+
+@user.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'GET':
-        return render_template('ozgun.html')
-    else:
+        return render_template('register.html')
+    a_user = User(request.form['reg_username'], request.form['reg_email'],
+                  generate_password_hash(request.form['reg_password']),
+                  request.form['reg_fullname'], request.form['reg_gender'])
+    try:
+        user.service.register_user(a_user)
+    except dbapi2.IntegrityError:
+        return render_template('register.html', error="Username or Email is already exist!")
+    return redirect(url_for('user.login'))
 
-        userObject = User(request.form['reg_username'], request.form['reg_email'], request.form['reg_password'],
-                          request.form['reg_fullname'], 'F','F')
-        user.service.add_user(userObject.username,userObject.email,userObject.password,userObject.fullname,'F','F')
-        return render_template('home.html')
+@user.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    a_user = user.service.get_user_by_username(request.form['lg_username'])
+    if a_user and check_password_hash(a_user.passwordHash, request.form['lg_password']):
+        remember = False
+        if "lg_remember" in request.form:
+            remember = True
+        print(request.form)
+        login_user(a_user, remember=remember)
+        return redirect(url_for('home_page'))
+    return render_template('login.html', error="Wrong password or username!")
 
+@user.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home_page'))
 
+@user.route('/users')
+def list():
+    users = user.service.list_all_users(current_user)
+    return render_template('users.html', users=users)
+
+@user.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'GET':
+        return render_template('settings.html')
+    current_user.username = request.form['reg_username']
+    current_user.email = request.form['reg_email']
+    if request.form['reg_password']:
+        current_user.passwordHash = generate_password_hash(request.form['reg_password'])
+    current_user.fullname = request.form['reg_fullname']
+    current_user.gender = request.form['reg_gender']
+    try:
+        user.service.update_user(current_user)
+    except dbapi2.IntegrityError:
+        return render_template('settings.html', error="Username or Email is already exist!")
+    return render_template('settings.html')
+
+@user.route('/delete')
+@login_required
+def delete():
+    id = current_user.id
+    logout_user()
+    user.service.delete_user(id)
+    return redirect(url_for('home_page'))
