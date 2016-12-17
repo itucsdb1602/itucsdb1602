6780929760426f8c3f173081c7d46fb54c7216ea
ozgunkivrakdal <ozgun.kivrakdal@gmail.com>
2016-12-17 16:31:19 +0200

    added id and auth logic coming from flask login plugin to our user model

--- user_class.py
+++ user_class.py
@@ -1,13 +1,23 @@
-class User:
+from flask_login import UserMixin
 
-    def __init__(self,username,email ="",password ="",fullname = "",
-                 gender = "",isBlocked =""):
-
+class User(UserMixin):
+    def __init__(self, username, email, passwordHash, fullname, gender):
+        self.id = None
         self.username = username
         self.email = email
-        self.password = password
+        self.passwordHash = passwordHash
         self.fullname = fullname
         self.gender = gender
-        self.isBlocked = isBlocked
+
+    @property
+    def is_authenticated(self):
+        if self.id:
+            return True
+        return False
+
+    def get_id(self):
+        return str(self.id).encode("utf-8").decode("utf-8")
 
 
+    def set_id(self, id):
+        self.id = id
