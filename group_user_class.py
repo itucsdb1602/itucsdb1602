6f26ae411294a66a0086a16a3139c22cc9d6c1f2
begulsen <bilalemre_gulsen@hotmail.com>
2016-12-18 04:34:48 +0200

    Same commit for crud operation for new group-user entity.
    Because of the wrong merge operation, some informations are lost.

--- group_user_class.py
+++
@@ -1,6 +0,0 @@
-class GroupUser:
-    def __init__(self,user_id,group_id,isAdmin, *args,**kwargs ):
-        self.user_id = user_id
-        self.group_id = group_id
-        self.isAdmin = isAdmin
-        self.user_name = kwargs.get('user_name',None)
